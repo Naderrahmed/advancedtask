66b0e844550f9da60649791b839f603b93758f82
Naderrahmed <45291667+Naderrahmed@users.noreply.github.com>
2018-11-23 16:53:51 +0200

    task 1

---
+++ task1/Controllers/Hom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace task1.Controllers
+{
+    public class Hom: Controller
+    {
+        // GET: /<controller>/
+        public string Index ()
+        {
+            return" this is my task1 " ;
+        }
+    }
+}
